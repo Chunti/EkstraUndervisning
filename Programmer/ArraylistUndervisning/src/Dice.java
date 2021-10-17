d1787f8b6d47358eb9c3cd5b80519a927476f56c
Zack Ottesen <61698597+Chunti@users.noreply.github.com>
2021-10-18 00:51:41 +0200

    -

--- Programmer/ArraylistUndervisning/src/Dice.java
+++ Programmer/ArraylistUndervisning/src/Dice.java
@@ -2,11 +2,11 @@ public class Dice {
     private int diceValue;
 
     public Dice() {
-        diceValue = (int) ((Math.random() * (7 - 1)) + 1);
+        diceValue = (int) (6*Math.random() + 1);
     }
 
     public void rollDiceValue() {
-        diceValue = (int) ((Math.random() * (7 - 1)) + 1);
+        diceValue = (int) (6*Math.random() + 1);
     }
 
     public int getDiceValue() {
